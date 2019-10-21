86ef04042e8f8d5dc9b13aaa8ac27aa71a369c00
gaon_choi <choigaon1028@gmail.com>
2019-10-22 01:45:20 +0900

    04주차_1__Open - Reading Files

---
+++ 04주차_데이터 불러오기 및 Numpy 라이브러리/04주차_1__Open - Reading Files/01_file_reading.py
@@ -0,0 +1,4 @@
+file1=open('C:\\Users\\choig\\Documents\\AIX0004\\04주차_데이터 불러오기 및 Numpy 라이브러리\\04주차_1__Open - Reading Files\\text_new.txt', 'r')
+print(file1.name)
+file_stuff=file1.read()
+print(file_stuff)
