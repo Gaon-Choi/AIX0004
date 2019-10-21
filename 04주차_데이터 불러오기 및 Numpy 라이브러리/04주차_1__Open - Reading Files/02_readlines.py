be0a3f7e86a5e9a861904a241e78009867551429
gaon_choi <choigaon1028@gmail.com>
2019-10-22 01:46:51 +0900

    eddited

--- 04주차_데이터 불러오기 및 Numpy 라이브러리/04주차_1__Open - Reading Files/02_readlines.py
+++ 04주차_데이터 불러오기 및 Numpy 라이브러리/04주차_1__Open - Reading Files/02_readlines.py
@@ -2,5 +2,6 @@ file1 = open(
     'C:\\Users\\choig\\Documents\\AIX0004\\04주차_데이터 불러오기 및 Numpy 라이브러리\\04주차_1__Open - Reading Files\\text_new.txt',
     'r')
 print(file1.name)
-file_stuff = file1.read()
+file_stuff = file1.readlines()
 print(file_stuff)
+# str이 아닌 리스트 형태로 개별 라인을 저장함.
--- 04주차_데이터 불러오기 및 Numpy 라이브러리/04주차_1__Open - Reading Files/readlines.py
+++
@@ -1,5 +0,0 @@
-file1=open('C:\\Users\\choig\\Documents\\AIX0004\\04주차_데이터 불러오기 및 Numpy 라이브러리\\04주차_1__Open - Reading Files\\text_new.txt', 'r')
-print(file1.name)
-file_stuff=file1.readlines()
-print(file_stuff)
-# str이 아닌 리스트 형태로 개별 라인을 저장함.
